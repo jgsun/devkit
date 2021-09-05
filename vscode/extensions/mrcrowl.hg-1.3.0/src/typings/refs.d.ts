852f63ec12c6cfe8d590246f4d1504829d079ca0
jgsun <jianguo_sun@hotmail.com>
2021-09-05 18:16:31 +0800

    vscode: delete extensions

--- vscode/extensions/mrcrowl.hg-1.3.0/src/typings/refs.d.ts
+++
@@ -1,9 +0,0 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Ben Crowl. All rights reserved.
- *  Original Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
-/// <reference path='../../node_modules/vscode/vscode.d.ts'/>
-/// <reference types='@types/node'/>
-/// <reference types='@types/tmp'/>
